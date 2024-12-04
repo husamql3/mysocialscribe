e5cd3ab03cb435a429f19a38761e26f5db19efdf
Hüsam <husamahmud@gmail.com>
2024-12-04 19:35:11 +0300

    fix: update redirection

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -3,27 +3,15 @@ import { NextResponse } from 'next/server'
 import { createClient } from '@/db/supabase/server'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
   const next = searchParams.get('next') ?? '/'
 
   if (code) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
-
-    if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host')
-      const isLocalEnv = process.env.NODE_ENV === 'development'
-
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
-    }
+    if (!error) return NextResponse.redirect(`https://mysocialscribe.com${next}`)
   }
 
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  return NextResponse.redirect('https://mysocialscribe.com/auth/auth-code-error')
 }
