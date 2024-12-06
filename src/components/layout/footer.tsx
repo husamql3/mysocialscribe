8e11b2175dd6910abc78ebda886bf1fd4906a351
Hüsam <husamahmud@gmail.com>
2024-12-06 21:48:30 +0300

    feat: update footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,26 +1,34 @@
 import { VERSION } from '@/constants/VERSION'
-import { LINKS } from '@/constants/LINKS'
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto flex w-full max-w-3xl flex-col-reverse items-center justify-between gap-4 px-4 py-6 md:flex-row md:px-0">
-      <div className="flex gap-2 text-sm">
-        <p>© {new Date().getFullYear()} mysocialscribe</p>
-        <span>-</span>
-        <p>v{VERSION}</p>
+    <footer className="container mx-auto flex w-full max-w-3xl flex-col-reverse items-center justify-between gap-2 px-4 py-6 text-gray-200 md:flex-row md:px-0">
+      <div className="text-sm font-light">
+        <p>
+          © {new Date().getFullYear()} MySocialScribe | v{VERSION}
+        </p>
       </div>
 
-      <div className="flex gap-4">
-        {LINKS.map((link) => (
-          <a
-            key={link.href}
-            href={link.href}
-            target="_blank"
-            className="transition-all hover:scale-110"
-          >
-            {link.icon}
-          </a>
-        ))}
+      <div className="space-x-1 text-center text-xs font-light text-gray-200">
+        <span>Crafted by</span>
+        <a
+          href="https://www.linkedin.com/in/husamahmud/"
+          target="_blank"
+          className="text-blue-500 transition duration-200 hover:underline"
+        >
+          @Hüsam
+        </a>
+
+        <br className="hidden md:block" />
+
+        <span>Mentored by</span>
+        <a
+          href="https://www.linkedin.com/in/julienbarbier/"
+          target="_blank"
+          className="text-blue-500 transition duration-200 hover:underline"
+        >
+          @Julien
+        </a>
       </div>
     </footer>
   )
