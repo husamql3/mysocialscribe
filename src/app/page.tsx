70a1e4f0d132fc19c888c73208d4d14c4e4a90e0
Hüsam <husamahmud@gmail.com>
2024-12-08 01:23:01 +0300

    fix: remove DlBtn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { createClient } from '@/db/supabase/server'
 import Title from '@/components/components/title'
 import Download from '@/components/components/download'
 import { CardStackComponent } from '@/components/components/card-stack-component'
-import { Button } from '@/components/ui/button'
-import { Download as DL } from 'lucide-react'
 
 const Home = async () => {
   const supabase = await createClient()
@@ -14,7 +12,6 @@ const Home = async () => {
   return (
     <main className="flex h-56 flex-1 flex-col items-center justify-center gap-8">
       <Title />
-      <DlBtn />
       <Download user={user} />
       <CardStackComponent />
     </main>
@@ -22,20 +19,3 @@ const Home = async () => {
 }
 
 export default Home
-
-export function DlBtn() {
-  return (
-    <Button
-      asChild
-      className="flex items-center gap-2"
-    >
-      <a
-        href="/downloads/twitter_space_98aed5a5.mp3"
-        download="twitter_space_98aed5a5.mp3"
-      >
-        <DL className="h-4 w-4" />
-        Download MP3
-      </a>
-    </Button>
-  )
-}
